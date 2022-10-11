7e3131995c2b3c197a5beb9ec9b077467f07ea77
MAC <mac@MACs-MacBook-Pro.local>
2022-10-11 15:56:36 +0500

    update workflow.yml file

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get("/", (req, res) => {
   res.send("HELLO WORLD");
 });
 
+app.get("/test", (req, res) => {
+  res.send("Hello TEST!");
+});
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
